1e8d8b9dc511eba3db30f2c1c9be239b19ceb49c
David <thexumaker@berkeley.edu>
2020-03-05 17:31:52 -0800

    Finished the calculator for a non white apprel

--- EthicalCalcFunctions.py
+++ EthicalCalcFunctions.py
@@ -3,3 +3,139 @@ spotColorDict = {"1C":[1.38,1.1,0.95,0.85,0.75,0.7], "2C":[1.63,1.28,1.1,0.95,0.
 "7C":[2.88,2.18,1.85,1.45,1.35,1.3], "8C":[3.13,2.36,2,1.55,1.45,1.4],
 "9C": [3.38,2.54,2.15,1.65,1.55,1.5], "10C":[3.63,2.72,2.3,1.75,1.65,1.6], "11C":[3.88,2.9,2.45,1.85,1.75,1.7],
 "12C":[4.13,3.08,2.6,1.95,1.85,1.8], "13C":[4.38,3.26,2.75,2.05,1.95,1.9], "14C" : [4.63,3.44,2.9,2.15,2.05,2]}
+#Calculates printing var, which determines price per shirt based on quantity
+import math
+
+def ceil(x, s):
+    return s * math.ceil(float(x)/s)
+def printingVar(quantity):
+    #Returns a single value
+
+    if quantity < 300:
+        return 0
+    elif quantity >= 300 and quantity < 500:
+        return 1
+    elif quantity >= 500 and quantity < 1000:
+        return 2
+    elif quantity >= 1000 and quantity < 2000:
+        return 3
+    elif quantity >= 2000 and quantity < 5000:
+        return 4
+    else:
+        return 5
+
+def referenceCalculator(printingVar):
+#Calculates the referene sheet used to get price per shirt
+#Returns Dictionary
+    reference = {}
+    for k,v in spotColorDict.items():
+        reference.update({k:v[printingVar]})
+    return reference
+def addC(str):
+    #Bro if u can't understand this lord help u
+    return str + "C"
+def subtotalPriceCalculator(listofLocations,white,reference):
+    #Returns a dictionary of subtotal prices
+    updatedList = []
+    subtotaldict = {"Front":None, "Back":None,"Left":None, "Right": None}
+    if not white:
+        for l in listofLocations:
+            l +=1
+            l = addC(str(l))
+            updatedList.append(l)
+    tempValue =0
+    for k,v in subtotaldict.items():
+        subtotaldict.update({k:reference.get(updatedList[tempValue])})
+        tempValue +=1
+    return subtotaldict
+
+def totalPriceCalculator(subtotal):
+    totalPricePerShirt = 0
+    for k,v in subtotal.items():
+        totalPricePerShirt += float(v)
+    return totalPricePerShirt
+
+def screenCalculator(listofLocations,quantity, oversized):
+    #Returns the total cost of screens
+    screenCost =0
+    if quantity > 1000:
+        screenCost = (oversized +1) * 32
+    else:
+        for loc in listofLocations:
+            if loc != 0:
+                screenCost += (loc +1) *28
+        screenCost += (oversized +1) * 32
+    return screenCost
+
+
+
+
+def flashCalculator(quantity,locations):
+    #Returns total cost of flash
+    #Returns a single value
+    return .15 * quantity * locations
+def underMinCalc(quan,loc):
+    if quan < 50:
+        return loc * 75
+    else:
+        return 0
+def apparelTotalCost(listofSubtotals, locations,quantity):
+    #calculates cost of apparel
+    tshirt = sum(listofSubtotals)
+    hoodie = tshirt + (.24 * locations * quantity)
+    zipped = tshirt + (1 * locations * quantity)
+    sleeve = tshirt + (.36 * locations * quantity)
+    pants = tshirt + (1.36 * locations * quantity)
+    priceDict = {"T-Shirts/Long-Sleeves Total":tshirt,
+"Hoodies/Crewnecks Total":hoodie,
+"Zipped Hoodies/Jackets/Windbreakers":zipped,
+"Printing on Sleeves/Pocket":sleeve,
+"Pants":pants}
+    return priceDict
+def shippingWithTax(apparelChoice, clothesDict):
+    costofChoice = clothesDict.get(apparelChoice)
+    rush = round(costofChoice * 1.25 * 1.0925,2)
+    superRush = round(costofChoice * 1.5 * 1.0925,2)
+    normal = round(costofChoice * 1.0925,2)
+    dict = {"Rush Shipping" : rush, "superRush": superRush, "Normal": normal}
+    return dict
+
+
+
+
+
+
+
+numLocations = 1
+nonWhiteApparel = False
+colorsF = 1
+colorsB = 2
+colorsR = 3
+colorsL = 4
+colorsSP = 1
+colorsOS = 1
+costPerItem = 1
+margin = 1.22
+quantity = int(input("Quantity of Apparel"))
+
+printingVar = printingVar(quantity)
+reference = referenceCalculator(printingVar)
+subTotal = subtotalPriceCalculator([colorsF,colorsB,colorsR, colorsL], nonWhiteApparel, reference)
+totalPricePerShirt = totalPriceCalculator(subTotal)
+cartTotalCost = totalPricePerShirt * quantity
+flash = flashCalculator(quantity,numLocations)
+screenTotal = screenCalculator([colorsF,colorsB,colorsR, colorsL],quantity,colorsOS)
+underminCharge = underMinCalc(quantity,numLocations)
+ClothesDict = apparelTotalCost([cartTotalCost,flash,screenTotal,underminCharge],numLocations,quantity)
+withTaxandShipping = shippingWithTax("T-Shirts/Long-Sleeves Total", ClothesDict)
+screenPrintingPlusBlank = (quantity * costPerItem )+ withTaxandShipping.get("Normal")
+preCutomerTotal = screenPrintingPlusBlank * margin
+customerPricePerItem = ceil(preCutomerTotal/ quantity,0.25)
+customerSubTotal = customerPricePerItem *quantity
+print(subTotal)
+print(totalPricePerShirt)
+print(screenTotal)
+print(ClothesDict)
+print(withTaxandShipping)
+print(customerPricePerItem)
+print(customerSubTotal)
